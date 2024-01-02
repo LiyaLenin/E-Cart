d3efcfba662751fa3931fab9f55e61e5e0ef0c99
Liya Lenin <liyaleninliya@gmail.com>
2024-01-02 21:25:49 +0530

    updatedcart

---
+++ src/Redux/cartStore.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import productSlice from "./Slices/productSlice";
+const cartStore=configureStore({
+    reducer:{
+productSlice
+    }
+})
+export default cartStore
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Col, Row, Button, Card } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { fetchProducts } from '../Redux/Slices/productSlice'
+import Spinner from 'react-bootstrap/Spinner';
 
 
 function Home() {
+  const dispatch = useDispatch()
+  const { loading, products, error } = useSelector((state) => state.productSlice)
+  useEffect(() => {
+    dispatch(fetchProducts())
+  }, [])
   return (
     <div style={{ marginTop: '60px' }}>
-      <Row className='mt-5 container'>
-        <Col className='mb-5' sm={12} md={6} lg={4} xl={3}>
-          <Card className='shadow rounded' style={{ width: '18rem' }}>
-            <Link to={'/view/1'}><Card.Img  style={{height:"300px"}} variant="top" src="https://img.buzzfeed.com/buzzfeed-static/static/2018-05/3/15/asset/buzzfeed-prod-web-03/sub-buzz-15015-1525376911-2.jpg?downsize=900:*&output-format=auto&output-quality=auto" /></Link>
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+      {
+        loading ? <div className='d-flex justify-content-center mt-5'><Spinner className='me-3' animation="border" variant="dark" />Loading....</div> :
+          <Row className='mt-5 container'>
+            {products.length>0&&products.map((product,index)=>(
+            <Col key={index} className='mb-5' sm={12} md={6} lg={4} xl={3}>
+              <Card className='shadow rounded' style={{ width: '18rem' }}>
+                <Link to={`/view/${product.id}`}><Card.Img style={{ height: "180px" }} variant="top" src={product.thumbnail} /></Link>
+                <Card.Body>
+                  <Card.Title>{product.title.slice(0,20)}...</Card.Title>
 
-              <div className='d-flex justify-content-between'>
-                <Button className='btn btn-light'><i className="fa-regular fa-heart text-danger "></i></Button>
-                <Button className='btn btn-light'><i className="fa-solid fa-cart-shopping text-success "></i></Button>
+                  <div className='d-flex justify-content-between'>
+                    <Button className='btn btn-light'><i className="fa-regular fa-heart text-danger "></i></Button>
+                    <Button className='btn btn-light'><i className="fa-solid fa-cart-shopping text-success "></i></Button>
 
-              </div>      </Card.Body>
-          </Card>
-        </Col >
-      </Row>
+                  </div>      </Card.Body>
+              </Card>
+            </Col >))}
+          </Row>
+      }
+
     </div>
   )
 }
